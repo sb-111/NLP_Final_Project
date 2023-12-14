b3732cfe1712258113fa6a580b079978ec804273
jin3811 <hjwoo1028@naver.com>
2023-12-14 15:12:20 +0900

    라디오 버튼 수평 배치

--- Service/main.py
+++ Service/main.py
@@ -4,19 +4,28 @@ from predict import inference_store_name
 
 st.set_page_config(page_title="O기요 리뷰 기반 매장 추천 서비스", page_icon="🍴", layout="centered")
 
-st.title("🍱 🍙별점 + 리뷰 기반 매장 추천 생성봇🍙 🍱")
+st.title("🍱 🍙별점 + 리뷰 기반 매장 추천 생성봇")
 
 st.markdown("안녕하세요! 저는 여러분들이 입력한 별점과 리뷰를 기반으로 한성대 인근 배달 음식점을 추천해주는 매장 추천 봇 입니다!", unsafe_allow_html=True)
 
 with st.form(key="customer_form", clear_on_submit=False):
 
+    # 수직 배치됨
     # star_cols = st.columns(3)
     # taste_star = star_cols[0].radio("맛 별점", ("5", "4", "3", "2", "1"))
     # quantity_star = star_cols[1].radio("양 별점", ("5", "4", "3", "2", "1"))
     # delivery_star = star_cols[2].radio("배달 별점", ("5", "4", "3", "2", "1"))
+
+    # 수평 배치
     taste_star = st.radio("맛 별점", ("5", "4", "3", "2", "1"))
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
     quantity_star = st.radio("양 별점", ("5", "4", "3", "2", "1"))
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
     delivery_star = st.radio("배달 별점", ("5", "4", "3", "2", "1"))
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
     customer_review = st.text_area("리뷰를 작성해주세요!", placeholder="리뷰를 입력해주세요.", key="text")
 
     submit = st.form_submit_button(label="별점/리뷰 기반 음식점 추천 시작하기")
